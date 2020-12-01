522402741f77988fe50f8c9ed24f64d5fceb2538
David Perlman <davidp@wix.com>
2020-12-01 11:57:48 +0200

    day 01 better solution

--- 5781/src/main/scala/com/advent/of/code/year2020/day01/Day01.scala
+++ 5781/src/main/scala/com/advent/of/code/year2020/day01/Day01.scala
@@ -2,23 +2,10 @@ package com.advent.of.code.year2020.day01
 
 object Day01 {
 
-  def solve1(input: Seq[Int]): Int = {
+  def solve1(input: Seq[Int]): Int =
+    input.combinations(2).find(_.sum == 2020).head.product
 
-    (for {
-      x <- input
-      y <- input if x + y == 2020
-    } yield x * y).head
-
-  }
-
-  def solve2(input: Seq[Int]): Int = {
-
-    (for {
-      x <- input
-      y <- input
-      z <- input if x + y + z == 2020
-    } yield x * y * z).head
-
-  }
+  def solve2(input: Seq[Int]): Int =
+    input.combinations(3).find(_.sum == 2020).head.product
 
 }
