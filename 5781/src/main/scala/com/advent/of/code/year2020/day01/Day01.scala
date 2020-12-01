90247eb4ce61b3fd4cba42881c220ef684cbcebf
David Perlman <davidp@wix.com>
2020-12-01 08:00:01 +0200

    day01 solution

--- 5781/src/main/scala/com/advent/of/code/year2020/day01/Day01.scala
+++ 5781/src/main/scala/com/advent/of/code/year2020/day01/Day01.scala
@@ -2,10 +2,23 @@ package com.advent.of.code.year2020.day01
 
 object Day01 {
 
-  object Part1 {
+  def solve1(input: Seq[Int]): Int = {
 
-    def solve(input: Seq[String]) = {
-      input.reverse
-    }
+    (for {
+      x <- input
+      y <- input if x + y == 2020
+    } yield x * y).head
+
   }
+
+  def solve2(input: Seq[Int]): Int = {
+
+    (for {
+      x <- input
+      y <- input
+      z <- input if x + y + z == 2020
+    } yield x * y * z).head
+
+  }
+
 }
--- 5781/src/test/scala/com/advent/of/code/year2020/day01/Day01Test.scala
+++ 5781/src/test/scala/com/advent/of/code/year2020/day01/Day01Test.scala
@@ -1,21 +1,63 @@
 package com.advent.of.code.year2020.day01
 
-import com.advent.of.code.year2020.day01.Day01.Part1.solve
+import com.advent.of.code.year2020.day01.Day01.{solve1, solve2}
+import org.specs2.matcher.Scope
 import org.specs2.mutable._
 
 import scala.io.Source
 
 class Day01Test extends SpecificationWithJUnit {
 
-  "Day One" should {
-    "solve part 1" in {
-
+  trait Context extends Scope {
+    val input: Seq[Int] = {
       val bufferedSource = Source.fromResource("day01/input.txt")
-      val input = bufferedSource.getLines.mkString.split(',').map(_.trim).toSeq
-      bufferedSource.close
-
-      solve(input) must beEqualTo(Seq("5", "4", "3", "2", "1"))
+      bufferedSource.getLines.toSeq.map(_.trim).filterNot(_ == "").map(_.toInt)
     }
   }
 
+  "Day One" should {
+
+    "Part One" should {
+      "Examples" should {
+
+        "solve first" in {
+          val input =
+            """
+              |1721
+              |979
+              |366
+              |299
+              |675
+              |1456""".stripMargin.split('\n').map(_.trim).filterNot(_ == "").map(_.toInt)
+
+          solve1(input) must_=== 514579
+        }
+      }
+
+      "solve it" in new Context {
+        solve1(input) must_=== 1016131
+      }
+    }
+
+    "Part Two" should {
+      "Examples" should {
+        "solve first" in {
+          val input =
+            """
+              |1721
+              |979
+              |366
+              |299
+              |675
+              |1456""".stripMargin.split('\n').map(_.trim).filterNot(_ == "").map(_.toInt)
+
+          solve2(input) must_=== 241861950
+        }
+      }
+
+      "solve it" in new Context {
+        solve2(input) must_=== 276432018
+      }
+    }
+  }
 }
