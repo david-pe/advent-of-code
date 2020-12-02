569986eaff47046d0df55251ad8b068cd1151c7d
David Perlman <davidp@wix.com>
2020-12-02 08:10:08 +0200

    day 02 solved

--- 5781/src/main/scala/com/advent/of/code/year2020/day02/Day02.scala
+++ 5781/src/main/scala/com/advent/of/code/year2020/day02/Day02.scala
@@ -2,10 +2,40 @@ package com.advent.of.code.year2020.day02
 
 object Day02 {
 
-  object Part1 {
+  def parseInput(input: Iterator[String]): Seq[(PasswordPolicy, String)] = {
+    val regex = raw"(\d*)-(\d*) (.): (.*)".r
 
-    def solve(input: Seq[String]) = {
-      input.reverse
+    input.map(_.trim).map {
+      case regex(min, max, char, pwd) => PasswordPolicy(min, max, char) -> pwd
+    }.toSeq
+  }
+
+  def solve1(input: Seq[(PasswordPolicy, String)]): Int = {
+
+    input.count {
+      case (PasswordPolicy(min, max, char), pwd) => {
+        val charCount = pwd.count(_ == char)
+        charCount >= min && charCount <= max
+      }
+    }
+
+  }
+
+  def solve2(input: Seq[(PasswordPolicy, String)]): Int = {
+
+    input.count {
+      case (PasswordPolicy(min, max, char), pwd) => {
+        val c1 = pwd.charAt(min - 1)
+        val c2 = pwd.charAt(max - 1)
+        (c1 == char || c2 == char) && !(c1 == char && c2 == char)
+      }
     }
   }
+
+  case class PasswordPolicy(min: Int, max: Int, char: Char)
+
+  object PasswordPolicy {
+    def apply(min: String, max: String, char: String): PasswordPolicy = PasswordPolicy(min.toInt, max.toInt, char.head)
+  }
+
 }
--- 5781/src/test/scala/com/advent/of/code/year2020/day02/Day02Test.scala
+++ 5781/src/test/scala/com/advent/of/code/year2020/day02/Day02Test.scala
@@ -1,21 +1,57 @@
 package com.advent.of.code.year2020.day02
 
-import com.advent.of.code.year2020.day01.Day01.Part1.solve
+import com.advent.of.code.year2020.day02.Day02.{parseInput, solve1, solve2}
+import org.specs2.matcher.Scope
 import org.specs2.mutable._
 
 import scala.io.Source
 
 class Day02Test extends SpecificationWithJUnit {
 
-  "Day Two" should {
-    "solve part 1" in {
-
+  trait Context extends Scope {
+    val input: Iterator[String] = {
       val bufferedSource = Source.fromResource("day02/input.txt")
-      val input = bufferedSource.getLines.mkString.split(',').map(_.trim).toSeq
-      bufferedSource.close
-
-      solve(input) must beEqualTo(Seq("1", "2", "3", "4", "5"))
+      bufferedSource.getLines
     }
   }
 
+  "Day Two" should {
+
+    "Part One" should {
+      "Examples" should {
+
+        "solve first" in {
+          val input =
+            """
+              |1-3 a: abcde
+              |1-3 b: cdefg
+              |2-9 c: ccccccccc""".stripMargin.split('\n').map(_.trim).filterNot(_ == "").toIterator;
+
+          solve1(parseInput(input)) must_=== 2
+        }
+      }
+
+      "solve it" in new Context {
+        solve1(parseInput(input)) must_=== 580
+      }
+    }
+
+    "Part Two" should {
+      "Examples" should {
+        "solve first" in {
+          val input =
+            """
+              |1-3 a: abcde
+              |1-3 b: cdefg
+              |2-9 c: ccccccccc""".stripMargin.split('\n').map(_.trim).filterNot(_ == "").toIterator;
+
+          solve2(parseInput(input)) must_=== 1
+        }
+
+        "solve it" in new Context {
+          solve2(parseInput(input)) must_=== 611
+        }
+      }
+    }
+  }
 }
