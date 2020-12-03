e1db02a8fa0df2e55252eafd88743887fad05831
David Perlman <davidp@wix.com>
2020-12-03 20:29:23 +0200

    refactor

--- 5781/src/main/scala/com/advent/of/code/year2020/day03/Day03.scala
+++ 5781/src/main/scala/com/advent/of/code/year2020/day03/Day03.scala
@@ -3,11 +3,12 @@ package com.advent.of.code.year2020.day03
 object Day03 {
 
   def solve2(input: Seq[String]): Long = {
-    Seq(Slope(1, 1)
-      , Slope(3, 1)
-      , Slope(5, 1)
-      , Slope(7, 1)
-      , Slope(1, 2))
+    Seq(Slope(1, 1),
+      Slope(3, 1),
+      Slope(5, 1),
+      Slope(7, 1),
+      Slope(1, 2)
+    )
       .map {
         case Slope(right, down) => solve1(input, right, down)
       }
@@ -16,7 +17,7 @@ object Day03 {
   }
 
   def solve1(input: Seq[String], right: Int = 3, down: Int = 1): Int = {
-    Range(0, input.length, down).zipWithIndex.count {
+    (input.indices by down).zipWithIndex.count {
       case (row, step) => didHitTree(input(row), step * right)
     }
   }
