c484f8d87be3e7de8f06b7c1fbb4c89856756ce0
David Perlman <davidp@wix.com>
2020-12-27 18:11:21 +0200

    refactor day 5

--- 5781/src/main/scala/com/advent/of/code/year2020/day05/Day05.scala
+++ 5781/src/main/scala/com/advent/of/code/year2020/day05/Day05.scala
@@ -2,16 +2,15 @@ package com.advent.of.code.year2020.day05
 
 object Day05 {
 
-  private val allSeats = (for {
-    row <- (0 to 127)
-    col <- (0 to 7)
-  } yield row * 8 + col).toSet
+  private val allSeats =  (0 to 127 * 8).toSet
 
   def solve2(input: Iterator[String]): Long = {
     val seats = input.map(getId).toSet
     val emptySeats = allSeats.diff(seats)
 
-    emptySeats.find(seat => seats.contains(seat + 1) && seats.contains(seat - 1)).get
+    emptySeats
+      .find(seat => seats.contains(seat + 1) && seats.contains(seat - 1))
+      .get
   }
 
   def solve1(input: Iterator[String]): Int = {
@@ -19,12 +18,11 @@ object Day05 {
   }
 
   private def getId(input: String) = {
-    val (row, col) = input
+    val seat = input
       .replaceAll("[FL]", "0")
       .replaceAll("[BR]", "1")
-      .splitAt(7)
 
-    b2d(row) * 8 + b2d(col)
+    b2d(seat)
   }
 
   private def b2d(binary: String) = Integer.parseInt(binary, 2)
