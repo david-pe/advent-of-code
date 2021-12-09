01dc1128b2b714ac0b62172369b3e46361961569
David Perlman <davidp@wix.com>
2021-12-09 15:18:11 +0200

    [2021] day 05 - small refactor

--- 5782/src/main/day05/tests.rs
+++ 5782/src/main/day05/tests.rs
@@ -16,45 +16,37 @@ fn example_data() -> String {
     )
 }
 
+fn read_lines(str: String) -> HashSet<Line> {
+    str.trim()
+        .split("\n")
+        .map(|line| Line::from_string(line))
+        .collect()
+}
+
 #[test]
 fn example_1() {
     let example = example_data();
-    let lines: HashSet<Line> = example
-        .split("\n")
-        .map(|line| Line::from_string(line))
-        .collect();
+    let lines = read_lines(example);
     assert_eq!(solve(lines), 5);
 }
 
 #[test]
 fn solve_it() {
     let file_data = Asset::read_input_file(module_path!());
-    let lines: HashSet<Line> = file_data
-        .trim()
-        .split("\n")
-        .map(|line| Line::from_string(line))
-        .collect();
+    let lines = read_lines(file_data);
     assert_eq!(solve(lines), 5690);
 }
 
 #[test]
 fn part_2_example_1() {
     let example = example_data();
-    let lines: HashSet<Line> = example
-        .split("\n")
-        .map(|line| Line::from_string(line))
-        .collect();
-
+    let lines: HashSet<Line> = read_lines(example);
     assert_eq!(solve_2(lines), 12);
 }
 
 #[test]
 fn part_2_solve_it() {
     let file_data = Asset::read_input_file(module_path!());
-    let lines: HashSet<Line> = file_data
-        .trim()
-        .split("\n")
-        .map(|line| Line::from_string(line))
-        .collect();
+    let lines = read_lines(file_data);
     assert_eq!(solve_2(lines), 17741);
 }
